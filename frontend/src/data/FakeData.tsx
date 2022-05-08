f455b6f8c3c0616de659448a9625a768b841dfa0
Timur Gareev <x517295@mail.muni.cz>
2022-05-08 17:11:17 +0200

    feat: added fake data for list items

---
+++ frontend/src/data/FakeData.tsx
@@ -0,0 +1,32 @@
+export const TemplateData = [
+    {
+        id: "1",
+        name: "That fucking burger"
+        // shopingItems: ShopingItemsData[0],
+        // steps: StepsData[0],
+    },
+    {
+        id: "2",
+        name: "Job interview",
+        // shopingItems: ShopingItemsData[1],
+        // steps: StepsData[1],
+    },
+    {
+        id: "3",
+        name: "Prepare homework",
+        // shopingItems: ShopingItemsData[2],
+        // steps: StepsData[2],
+    }
+];
+
+
+// export const ShopingItemsData: shoppingItemType = [
+//     {
+//     }
+// ];
+
+
+// export const StepsData: stepType = [
+//     {
+//     }
+// ];
