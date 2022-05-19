f212d1941d2be11cbf73700cd92c885e76e228ff
Dominika <xblehova@fi.muni.cz>
2022-05-19 20:20:23 +0200

    feat: add components for pop-up windows

---
+++ frontend/src/components/PopUpWindow.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "./Button";
+import "../styles/popUpWindow.css";
+
+interface PopUpWindowProps {
+  type: "order" | "template";
+}
+
+export const PopUpWindow: React.FC<PopUpWindowProps> = ( {type}: PopUpWindowProps) => {
+
+  const templateMode = type === "order" ? "template--invisible" : "";
+  const orderMode = type === "template" ? "order--invisible" : "";
+
+  function handleCancel(e: React.MouseEvent<HTMLInputElement>) {
+    e.preventDefault();
+}
+  
+  return (
+    <div className="popup-window">
+      <div className="popup__heading">
+        <span className="popup__heading--text">Do you want to save this {type}?</span>
+        <input className="cross-button" type="button" src="../images/cross.png" onClick={handleCancel}/>
+      </div>
+      <span className="popup__subheading">
+        Your changes will be lost
+      </span>
+      <div className="pupup__buttons">
+        <a className={`pupup__button ${orderMode}`}><Button size="wide" color="gray" label="Add to orders" /></a>
+        <a className={`pupup__button ${orderMode}`}><Button size="wide" color="gray" label="Save as a template" /></a>
+        <a className={`pupup__button ${templateMode}`}><Button size="wide" color="gray" label="Save changes" /></a>
+        <a className="pupup__button"><Button size="primary" color="orange" label="Delete" /></a>
+      </div>
+  </div>
+  );
+};
