248fa26e95afe4e044bfcc24d40199b4fd7081d5
Patrik Michal Vlcek <493059@mail.muni.cz>
2022-05-26 11:55:06 +0200

    feat: added basic error handling for order apis

--- backend/src/resources/order.ts
+++ backend/src/resources/order.ts
@@ -5,14 +5,21 @@ import {Request, Response} from 'express';
  * Return list of all orders
  */
 export const get = async (req: Request, res: Response) => {
-    const users = await prisma.order.findMany({
-        select: {
-            id: true,
-            orderBy: true,
-            createdAt: true,
-            shoppingList: true,
-        },
-    });
+    let users;
+    try {
+        users = await prisma.order.findMany({
+            select: {
+                id: true,
+                orderBy: true,
+                createdAt: true,
+                shoppingList: true,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Error")
+        return
+    }
     return res.send({
         status: 'success',
         data: users,
@@ -24,30 +31,37 @@ export const get = async (req: Request, res: Response) => {
  */
 export const store = async (req: Request, res: Response) => {
     const {orderBy, shoppingList, steps, createdAt} = req.body
-    const order = await prisma.order.create({
-        data: {
-            orderBy: orderBy,
-            shoppingList: {
-                create: {
-                    products: {
-                        createMany: {
-                            data: [
-                                ...shoppingList
-                            ]
+    let order;
+    try {
+        order = await prisma.order.create({
+            data: {
+                orderBy: orderBy,
+                shoppingList: {
+                    create: {
+                        products: {
+                            createMany: {
+                                data: [
+                                    ...shoppingList
+                                ]
+                            }
                         }
                     }
-                }
-            },
-            Steps: {
-                createMany: {
-                    data: [
-                        ...steps
-                    ]
-                }
-            },
-            createdAt: req.body.createdAt
-        }
-    })
+                },
+                Steps: {
+                    createMany: {
+                        data: [
+                            ...steps
+                        ]
+                    }
+                },
+                createdAt: req.body.createdAt
+            }
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Error")
+        return
+    }
     return res.send({
         status: "success",
         data: order
