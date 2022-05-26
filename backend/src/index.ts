b55d777110279c27b0bba8e4747a44b4931d9eec
Patrik Michal Vlcek <493059@mail.muni.cz>
2022-05-26 13:23:56 +0200

    feat: implemented shoppingList api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { order } from "./resources";
+import { shoppingList, order } from "./resources";
 import cors from "cors";
 
 const api = express();
@@ -10,5 +10,8 @@ api.use(cors());
 
 api.get("/order", order.get)
 api.post("/order", order.store)
+
+api.get("/shoppingList/:id", shoppingList.get)
+
 
 api.listen(port, () => console.log(`OrderHub app listening on port ${port}`));
---
+++ backend/src/resources/shoppingList.ts
@@ -0,0 +1,25 @@
+import prisma from '../client';
+import {Request, Response} from 'express';
+
+/**
+ * Return shoppingList with order id
+ */
+export const get = async (req: Request, res: Response) => {
+    const shoppingListId = req.params.id;
+    let shoppingItems;
+    try {
+        shoppingItems = await prisma.shoppingItem.findMany({
+            where: {
+                shoppingListId: shoppingListId
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Error")
+        return
+    }
+    return res.send({
+        status: 'success',
+        data: shoppingItems,
+    });
+}
