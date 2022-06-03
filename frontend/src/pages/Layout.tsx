14933c3ddc1b816452fc35dc28ab8de109b5517e
Timur Gareev <x517295@mail.muni.cz>
2022-06-03 11:04:27 +0200

    feat: added layout for pages

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Main } from "../components/Main";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import "./../styles/mainPage.css";
+import { Link } from "react-router-dom";
+import { List } from "../components/List";
 
-interface LayoutProps {
-
-}
-
-export const Layout: React.FC<LayoutProps> = (props) => {
+export const Layout: React.FC = () => {
 
     return (
-        <></>
+        <div>
+            <Header templates={true} title="Order Hub" orders={true} />
+            <aside>
+                <List editable={false} />
+            </aside>
+            <Main headingTitle="Heading Title" datetimeText="Date Time" type="order" />
+            <aside>
+                <List editable={false} />
+            </aside>
+            <Footer main_page={true} main_button="Add order" />
+        </div>
     );
+
 };
