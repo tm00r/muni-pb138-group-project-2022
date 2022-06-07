c06d91ff0d30adb7a395870c255192d943ae562d
Patrik Michal Vlcek <493059@mail.muni.cz>
2022-06-07 18:29:28 +0200

    feat: added update for steps api isFinished

--- backend/src/resources/steps.ts
+++ backend/src/resources/steps.ts
@@ -22,5 +22,55 @@ export const get = async (req: Request, res: Response) => {
         status: 'success',
         data: steps,
     });
+}
 
+/**
+ * Sets step to finished and evaluate order isFinished
+ */
+export const update = async (req: Request, res: Response) => {
+    const stepId = req.params.id;
+    let step;
+    let orderIsFinished = false
+    try {
+        step = await prisma.step.update({
+            where: {
+                id: stepId
+            },
+            data: {
+                isFinished: true
+            }
+        });
+        orderIsFinished = await evaluateOrderCompletion(step.orderId)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Error")
+        return
+    }
+    return res.send({
+        status: 'success',
+        data: step,
+        orderIsFinished: orderIsFinished
+    });
+}
+
+const evaluateOrderCompletion = async (orderId: string) => {
+    const steps = await prisma.step.findMany({
+        where: {
+            orderId: orderId
+        }
+    })
+    const areAllStepsFinished = steps.every(step => step.isFinished === true)
+    if (areAllStepsFinished === true) {
+        console.log("Steps finished for order " + orderId)
+        const order = await prisma.order.update({
+            where: {
+                id: orderId
+            },
+            data: {
+                isFinished: true
+            }
+        })
+        console.log("Order finished. Order id: " + order.id)
+    }
+    return areAllStepsFinished;
 }
